fcdaf8faff7ca18be31f0ddc461970808683f2bd
Tao LIN <nblintao@126.com>
2014-10-28 13:22:50 +0800

    Navigator layout

---
+++ 2-Browser/src/browser/BrowseListener.java
@@ -0,0 +1,14 @@
+package browser;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BrowseListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(e.getActionCommand());
+
+	}
+
+}
--- 2-Browser/src/browser/MyBrowser.java
+++ 2-Browser/src/browser/MyBrowser.java
@@ -1,11 +1,12 @@
 package browser;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class MyBrowser extends JFrame{
+	private static final long serialVersionUID = -579949100353474747L;
 	MyBrowser(){
 		super("MyBrowser");
 		
@@ -13,10 +14,50 @@ public class MyBrowser extends JFrame{
 		
 		Toolkit toolkit = getToolkit();
 		Dimension dimension = toolkit.getScreenSize();
-		setBounds(0,0,dimension.width,dimension.height);
+		setBounds(dimension.width/4,dimension.height/4,dimension.width/2,dimension.height/2); 
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		InitializeLayout();
+		
+	}
+	public void InitializeLayout(){
+		Box navigator = Box.createHorizontalBox();
+		
+		JButton buttonBackward = new JButton("Backward");
+		navigator.add(buttonBackward);
+
+		JButton buttonForward = new JButton("Forward");
+		navigator.add(buttonForward);
+		
+		JButton buttonRefresh = new JButton("Refresh");
+		navigator.add(buttonRefresh);
+
+//		BrowseListener browseListener = new BrowseListener();
+		JTextField urlField = new JTextField();
+//		urlField.addActionListener(browseListener);
+		urlField.addActionListener(
+				new ActionListener(){
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						System.out.println("brose: " + e.getActionCommand());						
+					}
+				}
+		);
+		
+		navigator.add(urlField);
+
+		Container content = getContentPane();
+		content.setLayout(new BorderLayout());
+		
+		content.add(navigator, BorderLayout.NORTH);
+
+		TextArea txt = new TextArea();
+
+		content.add(txt, BorderLayout.CENTER);
+
+//		JTextField stu = new JTextField();
+//		content.add(stu, BorderLayout.SOUTH);
 	}
 	public static void main(String[] argv){
 		MyBrowser myBrowser = new MyBrowser();
