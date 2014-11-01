df9fb63a2720a5752747c91935338e046af42019
Tao LIN <nblintao@126.com>
2014-11-01 22:55:03 +0800

    Reflesh the container & deal with exception

--- 2-Browser/src/browser/BrowseListener.java
+++ 2-Browser/src/browser/BrowseListener.java
@@ -20,27 +20,23 @@ public class BrowseListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		URL sourceURL;
+		pageView.removeAll();
 		try {
 			sourceURL = new URL(e.getActionCommand());
 			BufferedReader in = new BufferedReader(new InputStreamReader(sourceURL.openStream()));
 			String buf;
-			int count = 0;
-			while(!(null==(buf=in.readLine())) && count < 10){
+			while(!(null==(buf=in.readLine()))){
 				System.out.println(buf);
 				JTextArea text = new JTextArea(buf);
 				text.setEditable(false);
 				pageView.add(text);
-				count++;
-			}
+			}			
 			
-		} catch (MalformedURLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		} catch (Exception e1) {
+			pageView.add(new JTextArea("555~ I can't find "+ e.getActionCommand()));
 		}
-
+		
+		pageView.revalidate();
 	}
 
 }
