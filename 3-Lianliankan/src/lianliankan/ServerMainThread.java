bb49a98d0c0f68d5cc0e76ba5a8de7c8c9b7bc5d
Tao LIN <nblintao@126.com>
2015-01-02 23:17:29 +0800

    Conbine GameServer and ServerMainThread

--- 3-Lianliankan/src/lianliankan/ServerMainThread.java
+++ 3-Lianliankan/src/lianliankan/ServerMainThread.java
@@ -1,17 +1,37 @@
 package lianliankan;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerMainThread extends Thread {
+	ServerSocket server;
+//	Socket client;
+	DataCenter dc;
+
 	ServerMainThread(){
 		start();
 	}
 	public void run(){
-		GameServer gs = new GameServer();
 		try {
-			gs.start();
+			server = new ServerSocket(9999);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		System.out.println("Server start");
+		dc = new DataCenter();
+		while(true){
+			Socket client;
+			try {
+				client = server.accept();
+				dc.clientList.add(client);
+				System.out.println("Server accept new client");
+				ServerThread st = new ServerThread(dc, client);
+				st.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 }
